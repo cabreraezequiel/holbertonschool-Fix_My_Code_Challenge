23142f262368444989a3ee6af8d398c5b921f0b0
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-09-26 16:37:11 -0300

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
+
 class square():
 
     width = 0
     height = 0
-
 
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
@@ -20,10 +20,10 @@ class square():
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
-
 if __name__ == "__main__":
 
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
+    
